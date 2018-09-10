93dae675bdbfdaf91908ffac063a5aab3adb5d1e
Aashish Karki <aashish.y2z@gmail.com>
2018-09-10 12:11:50 +0545

    Remove unwanted files and project restructure

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/** @format */
+const Checkbox  = require('./src/CheckBox');
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+module.exports = Checkbox;
